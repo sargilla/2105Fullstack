c104672a661e249e049e98ce6bb3278523fc7a90
Santiago Argilla <sargilla@gmail.com>
2022-01-31 15:57:14 -0300

    inicio clase 11

--- clase_11_php/ejemplos.php
+++ clase_11_php/ejemplos.php
@@ -1,10 +1,1 @@
 <?php
-for ($i = 1; $i <= 10; $i++) {
-    if ($i == 3) {
-        continue; // Salta la iteración
-    }
-    echo "$i manzana\n"; # sentencia de impresión por pantalla
-    if ($i == 8) {
-        break; // Termina el bucle
-    }
-}
